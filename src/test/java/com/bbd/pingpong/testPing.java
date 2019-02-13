8afaca0d5ebb2f3b05fed43328f8a9a548e51f88
Dimpho Putu <dimphop@bbd.co.za>
2019-02-13 12:59:03 +0200

    boiii

---
+++ src/test/java/com/bbd/pingpong/testPing.java
@@ -0,0 +1,18 @@
+package com.bbd.pingpong;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testPing {
+
+    @ParameterizedTest
+    @CsvSource({"I, 1", "V, 5", "II, 2", "IV, 4", "V, 5" , "IX, 9", "X, 10", "L, 50", "C, 100", "D, 500", "M, 1000" })
+    public void testNumber1(String roman, int expected) {
+//        System.out.print('I');
+        assertEquals(expected,RomanConverter.Convert(roman));
+    }
+}
